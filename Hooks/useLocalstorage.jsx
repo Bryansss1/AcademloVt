c3f01d6f6d6804422ac35052b139ee130916833e
Bryansss1 <gregoria1540@gmail.com>
2022-11-10 15:16:36 -0400

    10

---
+++ Hooks/useLocalstorage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const useLocalstorage = ({key,initialValue}) => {
+    
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default useLocalstorage;
--- components/UsuariosAgg.jsx
+++ components/UsuariosAgg.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useEffect } from 'react';
 
 const UsuariosAgg = () => {
 
@@ -7,8 +8,26 @@ const [imgg,setImg]=useState("https://cdn-icons-png.flaticon.com/512/21/21104.pn
 const [descriptiongg,setDes]=useState("descripcion")
 const [ageis,setAge]=useState("edad")
 
+const [selectuser,setUsersele]=useState(null)
+
 const idramdon=Math.floor(Math.random()*1000000)+1
 const idramdon1=Math.floor(Math.random()*1000000)+1
+
+
+
+useEffect(()=>{
+if(selectuser){
+    setName(selectuser.name)
+    setImg(selectuser.img)
+    setAge(selectuser.age)
+    setDes(selectuser.description)
+}else{
+    setName("nombre")
+    setImg("https://cdn-icons-png.flaticon.com/512/21/21104.png")
+    setAge("edad")
+    setDes("descripcion")
+}
+},[selectuser])
 
 const notas=[{
     name:"Bryan Sanabria",
@@ -44,15 +63,46 @@ const [notes,setNotes]=useState(notas)
             img:imgg,
             id:idramdon
         } 
-        setNotes([...notes,Add])
+        if(selectuser!==null){
+            updatee(Add)
+        }else{
+            setNotes([...notes,Add])
+        }
        console.log(Add)
     }
+
 
    const deletee=(proid)=>{
         const filtrado=notes.filter(pro=>proid.id!==pro.id)
         setNotes(filtrado)
         console.log(proid.id)
    }
+
+   const selectProo=(selectuser)=>{
+    console.log(selectuser)
+    setUsersele(selectuser)
+   }
+   const updatee=(selectPro)=>{
+    selectPro.id=selectuser.id
+    console.log(selectPro.id)
+    const index=notes.findIndex(user=>user.id===selectPro.id)
+    notes[index]=selectPro
+    localStorage.setItem("notass",JSON.stringify(notes))
+    selectProo([...notes])
+    setUsersele(null)
+    console.log("actualizado")
+   }
+
+   useEffect(()=>{ 
+    let data=localStorage.getItem("notass")
+    if(data){
+       setNotes(JSON.parse(data))
+    }
+   },[])
+
+  useEffect(()=>{
+   localStorage.setItem("notass",JSON.stringify(notes))
+   },[notes])
 
     return (
         <div>
@@ -70,18 +120,21 @@ const [notes,setNotes]=useState(notas)
                     <li>Algo sobre mi:<br></br>{user.description}</li>
                     </ul>
                     <img src={user.img}/>
+                    <div className='butons'>
                     <button onClick={()=>deletee(user)}>delete</button>
+                    <button onClick={()=>selectProo(user)}>select</button>
+                    </div>
                     </div>
                 )
             })}
             </div>
             <div className='formula'>
             <form onSubmit={summmitchange}>
-                <input type="text" placeholder="tu nombre aqui..." onChange={inputName}/>
-                <input type="number"  placeholder="tu edad..." onChange={inputAge}/>
-                <input type="text" placeholder="tu descripcion de ti aqui..." onChange={inputDesc}/>
+                <input type="text" value={name} placeholder="tu nombre aqui..." onChange={inputName}/>
+                <input type="number" value={ageis}  placeholder="tu edad..." onChange={inputAge}/>
+                <input type="text" value={descriptiongg} placeholder="tu descripcion de ti aqui..." onChange={inputDesc}/>
                 <small>debes agg un vinculo compatible sin asteriscos en la seccion de img yan que el estado esta vacio con "" solo ,seria https.ect.png como ejemplo</small>
-                <input type="text" placeholder="tu bello rostro o avatar" onChange={inputImg}/>
+                <input type="text" value={imgg} placeholder="tu bello rostro o avatar" onChange={inputImg}/>
                 <button>Agg a la pagina:3</button>
             </form>
             </div>
